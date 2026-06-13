821e461260365c82b4467d4a3fe4692502f2e9fc
Neeraj Ojha <nojha1855@gmail.com>
2026-06-13 10:46:56 +0530

    add: visual dyanmic dashboard with working API endpoints

--- VIMS/urls.py
+++ VIMS/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from volunteer.views import DashboardView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="base.html"), name='home'),
+    path('', DashboardView.as_view(), name='home'),
     path('volunteer/', include('volunteer.urls')),
 ]
--- volunteer/views.py
+++ volunteer/views.py
@@ -1,7 +1,22 @@
 import csv
 from django.http import HttpResponse
 from .models import Volunteer, Task
+from django.views.generic import TemplateView
 from django.shortcuts import render
+
+class DashboardView(TemplateView):
+    template_name = 'dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['volunteers'] = Volunteer.objects.all().order_by('-joined_date')
+        context['total_volunteers'] = Volunteer.objects.count()
+        context['active_volunteers'] = Volunteer.objects.filter(status=True).count()
+        context['tasks'] = Task.objects.all().order_by('date')
+        context['total_tasks'] = Task.objects.count()
+
+        return context
+
 
 def export_volunteers_csv(request):
     response = HttpResponse(content_type='text/csv')
@@ -22,4 +37,3 @@ def export_volunteers_csv(request):
         ])
     
     return response
-        
